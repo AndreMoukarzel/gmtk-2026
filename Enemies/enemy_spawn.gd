fd427ff95ba2ae7d95ba052728040c6ea3aa810a
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2026-07-25 18:18:50 -0300

    Enemies spawn on calamities

--- Enemies/enemy_spawn.gd
+++ Enemies/enemy_spawn.gd
@@ -3,19 +3,14 @@ extends Area3D
 @export var ENEMY_SCN: PackedScene
 
 
-func _ready() -> void:
-	spawn_enemies(300)
-
-
 func spawn_enemies(amount: int) -> void:
 	for _i in range(amount):
 		var Enemy = ENEMY_SCN.instantiate()
 		var random_pos: Vector3 = random_point_in_area(self)
 		
 		Enemy.Player = $"../Player"
-		Enemy.global_position = Vector3(random_pos.x, 0.2, random_pos.z)
-		
 		$"../Enemies".add_child(Enemy)
+		Enemy.global_position = Vector3(random_pos.x, 1.0, random_pos.z)
 
 
 func random_point_in_area(area: Area3D) -> Vector3:
@@ -31,3 +26,7 @@ func random_point_in_area(area: Area3D) -> Vector3:
 	)
 
 	return collision_shape.global_transform * local_point
+
+
+func _on_calamity_controller_calamity_started(instance_id: int, calamity: CalamityData) -> void:
+	spawn_enemies(50)
